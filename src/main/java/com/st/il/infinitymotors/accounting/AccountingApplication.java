d135a5721f8b970ee174014332889cefeb9bb010
vyeung <vl.yeung@yahoo.com>
2019-06-24 15:44:12 -0400

    add JavaMailSender bean

--- src/main/java/com/st/il/infinitymotors/accounting/AccountingApplication.java
+++ src/main/java/com/st/il/infinitymotors/accounting/AccountingApplication.java
@@ -1,13 +1,42 @@
 package com.st.il.infinitymotors.accounting;
 
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @SpringBootApplication
 public class AccountingApplication {
 
+	//inject these values from application.properties
+	@Value("${spring.mail.username}")
+	String gmailUserName;
+	@Value("${spring.mail.password}")
+	String gmailPassword;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AccountingApplication.class, args);
 	}
 
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	    mailSender.setHost("smtp.gmail.com");
+	    mailSender.setPort(587);
+	     
+	    mailSender.setUsername(gmailUserName);
+	    mailSender.setPassword(gmailPassword);
+	     
+	    Properties props = mailSender.getJavaMailProperties();
+	    props.put("mail.transport.protocol", "smtp");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", "true");
+	    //props.put("mail.debug", "true");
+	     
+	    return mailSender;
+	}
 }
